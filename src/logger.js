52de987eacc6414fd95e8a622da52dcabecb6887
Frederic Heem <frederic.heem@gmail.com>
2020-05-16 11:26:21 +0100

    remove log files at startup

--- src/logger.js
+++ src/logger.js
@@ -1,21 +1,25 @@
-//const logger = require("pino")({ prettyPrint: true, level: "debug" });
 const { createLogger, format, transports } = require("winston");
+const fs = require("fs");
+
+const transportFiles = [
+  {
+    filename: "grucloud-debug.log",
+    level: "debug",
+    format: format.simple(),
+  },
+  {
+    filename: "grucloud-info.log",
+    level: "info",
+    format: format.simple(),
+  },
+];
+
+transportFiles.forEach((item) => fs.unlinkSync(item.filename));
+
 const logger = createLogger({
   level: "debug",
   format: format.combine(format.splat(), format.simple()),
-  //defaultMeta: { service: "user-service" },
-  transports: [
-    new transports.File({
-      filename: "grucloud-debug.log",
-      level: "debug",
-      format: format.simple(),
-    }),
-    new transports.File({
-      filename: "grucloud-info.log",
-      level: "info",
-      format: format.simple(),
-    }),
-  ],
+  transports: transportFiles.map((item) => new transports.File(item)),
 });
 
 module.exports = ({ prefix = "" }) => {
