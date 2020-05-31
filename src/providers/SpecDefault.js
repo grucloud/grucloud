7bdff5d072a4d255fc237123210f3dfb59484e83
Frederic Heem <frederic.heem@gmail.com>
2020-05-31 22:58:38 +0100

    remove dead code compare

--- src/providers/SpecDefault.js
+++ src/providers/SpecDefault.js
@@ -4,17 +4,6 @@ const assert = require("assert");
 const logger = require("../logger")({ prefix: "SpecDefault" });
 const compareObject = require("../Utils").compare;
 
-const compare = ({ target, live }) => {
-  logger.debug(`compare default`);
-  const diff = compareObject({
-    target,
-    targetKeys: Object.getOwnPropertyNames(target),
-    live,
-  });
-  logger.debug(`compare ${toString(diff)}`);
-  return diff;
-};
-
 exports.SpecDefault = () => ({
   compare: compareObject,
   //TODO move that to client ?
