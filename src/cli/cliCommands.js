eaa2f41f1e8f6a64d3e26005b70a97b5af66467a
Frederic Heem <frederic.heem@gmail.com>
2020-06-08 22:04:57 +0100

    countDestroyed, countResources

--- src/cli/cliCommands.js
+++ src/cli/cliCommands.js
@@ -2,8 +2,18 @@ const _ = require("lodash");
 const { runAsyncCommand } = require("./cliUtils");
 const { displayPlan, displayLive } = require("./displayUtils");
 const prompts = require("prompts");
-const { map, pipe, switchCase, any } = require("rubico");
+const { map, pipe, switchCase, any, reduce } = require("rubico");
 const { flatten, tap, isEmpty, pluck, ifElse } = require("ramda");
+
+const countResources = reduce(
+  (acc, value) => {
+    return {
+      create: acc.create + value.plan.newOrUpdate.length,
+      destroy: acc.destroy + value.plan.destroy.length,
+    };
+  },
+  { create: 0, destroy: 0 }
+);
 
 //Query Plan
 const planQuery = async ({ infra }) =>
@@ -23,8 +33,10 @@ exports.planQuery = planQuery;
 
 //Deploy plan
 exports.planDeploy = async ({ infra }) => {
-  const hasPlans = ({ plan }) =>
-    any((plan) => !isEmpty(plan.newOrUpdate) || !isEmpty(plan.destroy));
+  const hasPlans = pipe([
+    countResources,
+    ({ create, destroy }) => create > 0 || destroy > 0,
+  ]);
 
   const processNoPlan = () => {
     console.log("Nothing to deploy");
@@ -35,16 +47,30 @@ exports.planDeploy = async ({ infra }) => {
   };
 
   const promptConfirmDeploy = async (allPlans) => {
-    //console.log("promptConfirmDeploy", allPlans);
-    // Count resources to deploy and destroy
-    const { confirmDeploy } = await prompts({
-      type: "confirm",
-      name: "confirmDeploy",
-      message: "Are you sure to deploy these resources ?",
-      initial: false,
-    });
-    return confirmDeploy;
+    return await pipe([
+      countResources,
+      async ({ create, destroy }) =>
+        await prompts({
+          type: "confirm",
+          name: "confirmDeploy",
+          message: `Are you sure to deploy ${create}${
+            destroy > 0 ? ` and destroy ${destroy}` : ""
+          } resource(s) ?`,
+          initial: false,
+        }),
+      ({ confirmDeploy }) => confirmDeploy,
+    ])(allPlans);
   };
+
+  const displayResourcesDeployed = pipe([
+    countResources,
+    ({ create, destroy }) =>
+      console.log(
+        `${create} deployed${
+          destroy > 0 ? ` and ${destroy} destroyed` : ""
+        } resource(s)`
+      ),
+  ]);
 
   const doPlanDeploy = pipe([
     //tap((x) => console.log("doPlanDeploy", x)),
@@ -55,12 +81,11 @@ exports.planDeploy = async ({ infra }) => {
       );
       return { provider, plan };
     },
-    //tap((x) => console.log("deployPlan", x)),
   ]);
 
-  const doPlansDeploy = (plans) => map(doPlanDeploy)(plans);
+  const doPlansDeploy = pipe([map(doPlanDeploy), displayResourcesDeployed]);
 
-  const processPlans = switchCase([
+  const processDeployPlans = switchCase([
     promptConfirmDeploy,
     doPlansDeploy,
     abortDeploy,
@@ -68,8 +93,7 @@ exports.planDeploy = async ({ infra }) => {
 
   await pipe([
     planQuery,
-    //tap((x) => console.log("planQuery", x)),
-    switchCase([hasPlans, processPlans, processNoPlan]),
+    switchCase([hasPlans, processDeployPlans, processNoPlan]),
     //tap((x) => console.log("switchCase", x)),
   ])({ infra });
 };
@@ -82,33 +106,43 @@ exports.planDestroy = async ({ infra, options }) => {
     console.log("No resources to destroy");
   };
 
+  const countDestroyed = reduce((acc, value) => {
+    return acc + value.plan.length;
+  }, 0);
+
   const displayResourcesDestroyed = pipe([
-    flatten,
-    (plans) => console.log(`${plans.length} Resource(s) destroyed`),
+    countDestroyed,
+    (length) => console.log(`${length} Resource(s) destroyed`),
   ]);
 
-  const promptConfirmDestroy = async () => {
-    const { confirmDestroy } = await prompts({
-      type: "confirm",
-      name: "confirmDestroy",
-      message: "Are you sure to destroy these resources ?",
-      initial: false,
-    });
-    return confirmDestroy;
+  const promptConfirmDestroy = async (plans) => {
+    return await pipe([
+      countDestroyed,
+      async (length) =>
+        await prompts({
+          type: "confirm",
+          name: "confirmDestroy",
+          message: `Are you sure to destroy these ${length} resources ?`,
+          initial: false,
+        }),
+      ({ confirmDestroy }) => confirmDestroy,
+    ])(plans);
   };
 
-  const doPlansDestroy = pipe([
-    async (result) =>
-      map(async ({ provider, plan }) => {
-        return await runAsyncCommand(
-          () => provider.destroyPlan(plan),
-          `Destroying Resources on provider ${provider.name()}`
-        );
-      })(result),
-    displayResourcesDestroyed,
+  const doPlanDestroy = pipe([
+    //tap((x) => console.log("doPlanDeploy", x)),
+    async ({ provider, plan }) => {
+      await runAsyncCommand(
+        () => provider.destroyPlan(plan),
+        `Destroying ${plan.length} resource(s) on provider ${provider.name()}`
+      );
+      return { provider, plan };
+    },
   ]);
 
-  const processPlans = switchCase([
+  const doPlansDestroy = pipe([map(doPlanDestroy), displayResourcesDestroyed]);
+
+  const processDestroyPlans = switchCase([
     promptConfirmDestroy,
     doPlansDestroy,
     () => {
@@ -136,7 +170,7 @@ exports.planDestroy = async ({ infra, options }) => {
   await pipe([
     async (providers) => await map(findDestroy)(providers),
     //tap((x) => console.log(JSON.stringify(x, null, 4))),
-    ifElse(hasEmptyPlan, processHasNoPlan, processPlans),
+    ifElse(hasEmptyPlan, processHasNoPlan, processDestroyPlans),
   ])(infra.providers);
 };
 
