f1354d7023af8d3d88533fc100fedce729390bf8
Frederic Heem <frederic.heem@gmail.com>
2020-07-12 19:11:20 +0100

    AwsS3BucketErrors

---
+++ src/providers/aws/test/AwsS3Bucket.error.test.js
@@ -0,0 +1,34 @@
+const assert = require("assert");
+const { ConfigLoader } = require("ConfigLoader");
+const AwsProvider = require("../AwsProvider");
+const { testPlanDeploy, testPlanDestroy } = require("test/E2ETestUtils");
+
+describe.only("AwsS3BucketErrors", async function () {
+  let config;
+  before(async function () {
+    try {
+      config = ConfigLoader({ baseDir: __dirname });
+    } catch (error) {
+      this.skip();
+    }
+  });
+  after(async () => {});
+
+  it("s3Bucket already exist", async function () {
+    const provider = await AwsProvider({
+      name: "aws",
+      config: ConfigLoader({ baseDir: __dirname }),
+    });
+    await provider.makeS3Bucket({
+      name: "bucket",
+      properties: () => ({}),
+    });
+
+    try {
+      await provider.planQueryAndApply();
+      assert(false, "should not be here");
+    } catch (error) {
+      assert.equal(error.code, "Forbidden");
+    }
+  });
+});
