c7410c02ca66d522ce85981c2575b19d017c74ff
Frederic Heem <frederic.heem@gmail.com>
2020-09-01 17:20:54 +0200

    do not skip multi example

--- examples/multi/iac.multi.test.js
+++ examples/multi/iac.multi.test.js
@@ -3,7 +3,7 @@ const { ConfigLoader } = require("ConfigLoader");
 const cliCommands = require("../../src/cli/cliCommands");
 const { createStack } = require("./iac");
 
-describe.skip("Multi Example", async function () {
+describe("Multi Example", async function () {
   let config;
 
   before(async function () {
