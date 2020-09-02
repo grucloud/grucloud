6bfc02260558e69f8d608d2bf906ce678c727953
Frederic Heem <frederic.heem@gmail.com>
2020-09-02 10:39:44 +0200

    no aws and az for now

--- examples/multi/iac.js
+++ examples/multi/iac.js
@@ -138,8 +138,8 @@ exports.createStack = async ({ config }) => {
   return {
     providers: [
       await createMock({ config }),
-      await createAws({ config }),
-      await createAzure({ config }),
+      //await createAws({ config }),
+      //await createAzure({ config }),
       await createGoogle({ config }),
       //await createScaleway({ config }),
     ],
