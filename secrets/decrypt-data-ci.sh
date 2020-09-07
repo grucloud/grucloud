a81b60fcd56a649f42dcd025c3eee0204633e9c7
Frederic Heem <frederic.heem@gmail.com>
2020-09-07 09:37:57 -0300

    kp.pen.enc => kp.pem.enc

--- secrets/decrypt-data-ci.sh
+++ secrets/decrypt-data-ci.sh
@@ -1,4 +1,4 @@
 
-openssl aes-256-cbc -md sha256 -d -out secrets/kp.pem -in secrets/kp.pen.enc -pass pass:$KEY
+openssl aes-256-cbc -md sha256 -d -out secrets/kp.pem -in secrets/kp.pem.enc -pass pass:$KEY
 openssl aes-256-cbc -md sha256 -d -out examples/multi/config/default.env -in examples/multi/config/default.env.enc -pass pass:$KEY
 openssl aes-256-cbc -md sha256 -d -out examples/multi/config/grucloud-e2e-f35e5f0a014a.json -in examples/multi/config/grucloud-e2e-f35e5f0a014a.json.enc -pass pass:$KEY
