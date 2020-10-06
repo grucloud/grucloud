5740ab662b3d31b80ee56a2763417f3f844ca0f8
Frederic Heem <frederic.heem@gmail.com>
2020-10-06 07:57:56 -0300

    default.env is now in secrets

--- secrets/decrypt-data-ci.sh
+++ secrets/decrypt-data-ci.sh
@@ -1,5 +1,7 @@
 
 openssl aes-256-cbc -md sha256 -d -out secrets/kp.pem -in secrets/kp.pem.enc -pass pass:$KEY
-openssl aes-256-cbc -md sha256 -d -out examples/multi/config/default.env -in examples/multi/config/default.env.enc -pass pass:$KEY
+openssl aes-256-cbc -md sha256 -d -out secrets/default.env -in secrets/default.env.enc -pass pass:$KEY
 mkdir -p $HOME/.config/gcloud/
+cp secrets/default.env examples/multi/config/
+cp secrets/default.env examples/azure/config/
 openssl aes-256-cbc -md sha256 -d -out $HOME/.config/gcloud/grucloud.json  -in secrets/grucloud.json.enc -pass pass:$KEY
--- secrets/encrypt-data-ci.sh
+++ secrets/encrypt-data-ci.sh
@@ -1,4 +1,4 @@
 
 openssl aes-256-cbc -md sha256 -e -in secrets/kp.pem -out secrets/kp.pem.enc -k $KEY
-openssl aes-256-cbc -md sha256 -e -in examples/multi/config/default.env -out examples/multi/config/default.env.enc -k $KEY
+openssl aes-256-cbc -md sha256 -e -in secrets/default.env -out secrets/default.env.enc -k $KEY
 openssl aes-256-cbc -md sha256 -e -in $HOME/.config/gcloud/grucloud.json -out secrets/grucloud.json.enc -k $KEY
