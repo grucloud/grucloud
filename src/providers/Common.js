c87f31e6ef11e19aca2f1e1eedfbf2d67f4275f5
Frederic Heem <frederic.heem@gmail.com>
2020-10-26 09:58:51 -0300

    remove provider from getList

--- src/providers/Common.js
+++ src/providers/Common.js
@@ -104,14 +104,13 @@ exports.findField = ({ item, field }) => {
   }
 };
 
-exports.getByNameCore = async ({ provider, name, findName, getList }) => {
+exports.getByNameCore = async ({ name, findName, getList }) => {
   logger.info(`getByName ${name}`);
   assert(name, "name");
   assert(findName, "findName");
   assert(getList, "getList");
-  assert(provider, "provider");
 
-  const { items } = await getList({ provider });
+  const { items } = await getList();
   const instance = items.find((item) => findName(item) === name);
   logger.debug(`getByName ${name}: ${tos({ instance })}`);
 
