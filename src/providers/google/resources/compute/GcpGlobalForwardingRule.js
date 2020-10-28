75effda51d688c3cc1bb96bdabc9635b0d609394
Frederic Heem <frederic.heem@gmail.com>
2020-10-28 11:46:46 -0300

    globalForwardingRule isInstanceUp

--- src/providers/google/resources/compute/GcpGlobalForwardingRule.js
+++ src/providers/google/resources/compute/GcpGlobalForwardingRule.js
@@ -3,6 +3,7 @@ const { defaultsDeep } = require("rubico/x");
 const GoogleClient = require("../../GoogleClient");
 const { GCP_COMPUTE_BASE_URL } = require("./GcpComputeCommon");
 const { getField } = require("../../../ProviderCommon");
+const { isUpByIdCore } = require("../../../Common");
 
 // https://cloud.google.com/compute/docs/reference/rest/v1/globalForwardingRules
 exports.GcpGlobalForwardingRule = ({ spec, config }) => {
@@ -21,11 +22,22 @@ exports.GcpGlobalForwardingRule = ({ spec, config }) => {
     })(properties);
   };
 
+  const isInstanceUp = (instance) => {
+    return !!instance.IPAddress;
+  };
+
+  const isUpByIdFactory = ({ getById }) =>
+    isUpByIdCore({
+      isInstanceUp,
+      getById,
+    });
+  // Up when IPAddress is valid
   return GoogleClient({
     spec,
     baseURL: GCP_COMPUTE_BASE_URL,
     url: `/projects/${project}/global/forwardingRules`,
     config,
     configDefault,
+    isUpByIdFactory,
   });
 };
