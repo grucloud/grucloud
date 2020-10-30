6ca52550a79c3d74ef687f07dd808499da66f103
Frederic Heem <frederic.heem@gmail.com>
2020-10-30 12:26:57 -0300

    remove shouldRetryOnException in UrlMap

--- src/providers/google/resources/compute/GcpUrlMap.js
+++ src/providers/google/resources/compute/GcpUrlMap.js
@@ -27,8 +27,5 @@ exports.GcpUrlMap = ({ spec, config }) => {
     url: `/projects/${project}/global/urlMaps`,
     config,
     configDefault,
-    shouldRetryOnException: (error) => {
-      return error.response?.status === 404;
-    },
   });
 };
