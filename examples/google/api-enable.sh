62a2f5f8d6b81aaa1f509ca1d7a31745e7e76cd4
Frederic Heem <frederic.heem@gmail.com>
2020-11-04 12:33:52 -0300

    gcloud services enable dns.googleapis.com

--- examples/google/api-enable.sh
+++ examples/google/api-enable.sh
@@ -4,3 +4,4 @@ gcloud services enable cloudresourcemanager.googleapis.com
 gcloud services enable iam.googleapis.com
 gcloud services enable compute.googleapis.com
 gcloud services enable serviceusage.googleapis.com
+gcloud services enable dns.googleapis.com
