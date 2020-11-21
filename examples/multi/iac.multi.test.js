604fcd222978c153c2a2c96ca095d2a88978db09
Frederic Heem <frederic.heem@gmail.com>
2020-11-21 20:12:46 -0300

    multi example does not delete all

--- examples/multi/iac.multi.test.js
+++ examples/multi/iac.multi.test.js
@@ -18,7 +18,7 @@ describe("Multi Example", async function () {
 
     await cliCommands.planDestroy({
       infra,
-      commandOptions: { force: true, all: true },
+      commandOptions: { force: true },
     });
     await cliCommands.planQuery({ infra });
     await cliCommands.planApply({
