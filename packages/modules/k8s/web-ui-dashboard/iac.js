d8599d53cacc018f7863629e061724582b3f4f98
Frederic Heem <frederic.heem@gmail.com>
2021-04-03 15:51:40 -0500

    @grucloud/module-k8s-web-ui-dashboard

---
+++ packages/modules/k8s/web-ui-dashboard/iac.js
@@ -0,0 +1,8 @@
+const { K8sProvider } = require("@grucloud/provider-k8s");
+const { createResources } = require("./resources");
+
+exports.createStack = async () => {
+  const provider = K8sProvider({});
+  const resources = await createResources({ provider });
+  return { provider, resources };
+};
