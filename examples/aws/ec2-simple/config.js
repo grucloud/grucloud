45befa77b572cb5e4ac5e736b036737ae742e276
Frederic Heem <frederic.heem@gmail.com>
2021-04-14 22:36:06 -0500

    update AwsGettingStarted with ec2-simple

---
+++ examples/aws/ec2-simple/config.js
@@ -0,0 +1,11 @@
+const pkg = require("./package.json");
+module.exports = ({ stage }) => ({
+  projectName: pkg.name,
+  ec2Instance: {
+    name: "web-server",
+    properties: () => ({
+      InstanceType: "t2.micro",
+      ImageId: "ami-00f6a0c18edb19300", // Ubuntu 18.04
+    }),
+  },
+});
