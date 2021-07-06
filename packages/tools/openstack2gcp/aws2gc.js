fedaa3abf6304a551fb15c71bf59be82a7afb878
Frederic Heem <frederic.heem@gmail.com>
2021-07-06 18:30:49 +0200

    generatorMain createProgramOptions

--- packages/tools/openstack2gcp/aws2gc.js
+++ packages/tools/openstack2gcp/aws2gc.js
@@ -1,116 +1,18 @@
 #!/usr/bin/env node
 const assert = require("assert");
-const {
-  pipe,
-  tap,
-  get,
-  eq,
-  map,
-  flatMap,
-  fork,
-  switchCase,
-  filter,
-  and,
-  tryCatch,
-  not,
-  assign,
-} = require("rubico");
-const {
-  first,
-  find,
-  callProp,
-  pluck,
-  identity,
-  values,
-  flatten,
-  size,
-  defaultsDeep,
-} = require("rubico/x");
-const { Command } = require("commander");
+const { createProgramOptions, generatorMain } = require("./generatorUtils");
 
-const { readModel, readMapping, writeToFile } = require("./generatorUtils");
-
+const { configTpl } = require("./src/configTpl");
 const { iacTpl } = require("./src/aws/iacTpl");
 const { writeVpcs } = require("./src/aws/ec2/vpc/vpcGen");
 const { writeSubnets } = require("./src/aws/ec2/subnet/subnetGen");
 
-const createProgram = ({ version }) => {
-  const program = new Command();
-  program.storeOptionsAsProperties(false);
-  program.allowUnknownOption(); // For testing
-  program.version(version);
-  program.requiredOption("-i, --input <file>", "lives resources");
-  program.option("-o, --outputCode <file>", "iac.js output", "iac.js");
-  program.option("-c, --outputConfig <file>", "config.js output", "config.js");
-  program.option("-m, --mapping <file>", "mapping file", "mapping.json");
-
-  program.parse(process.argv);
-
-  return program;
-};
-
 const writers = [writeVpcs, writeSubnets];
 
-const configTpl = (content) => `const pkg = require("./package.json");
-module.exports = ({ stage }) => ({
-  projectName: pkg.name,
-  ${content}
-});`;
-
-const writeIac =
-  ({ filename }) =>
-  (resources) =>
-    pipe([
-      () => resources,
-      fork({
-        resourcesVarNames: pluck("resourceVarName"),
-        resourcesCode: pipe([pluck("code"), callProp("join", "\n")]),
-      }),
-      tap((xxx) => {
-        assert(true);
-      }),
-      ({ resourcesVarNames, resourcesCode }) =>
-        iacTpl({ resourcesVarNames, resourcesCode }),
-      writeToFile({ filename }),
-    ])();
-
-const writeConfig =
-  ({ filename }) =>
-  (resources) =>
-    pipe([
-      () => resources,
-      pluck("config"),
-      callProp("join", "\n"),
-      tap((xxx) => {
-        assert(true);
-      }),
-      configTpl,
-      writeToFile({ filename }),
-    ])();
-
-const main = async (options) =>
-  pipe([
-    tap((xxx) => {
-      console.log("aws2gc");
-    }),
-    fork({ lives: readModel(options), mapping: () => ({}) }),
-    ({ lives, mapping }) =>
-      flatMap((writeResource) => writeResource({ lives, mapping }))(writers),
-    filter(identity),
-    tap((xxx) => {
-      assert(true);
-    }),
-    fork({
-      iac: writeIac({ filename: options.outputCode }),
-      config: writeConfig({ filename: options.outputConfig }),
-    }),
-  ])();
-
 //TODO read version from package.json
-const program = createProgram({ version: "1.0" });
-const options = program.opts();
+const options = createProgramOptions({ version: "1.0" });
 
-main(options)
+generatorMain({ name: "aws2gc", options, writers, iacTpl, configTpl })
   .then(() => {})
   .catch((error) => {
     console.error("error");
--- packages/tools/openstack2gcp/generatorUtils.js
+++ packages/tools/openstack2gcp/generatorUtils.js
@@ -3,10 +3,19 @@ const path = require("path");
 const fs = require("fs").promises;
 const { camelCase } = require("change-case");
 const prettier = require("prettier");
-const { pipe, tap, get, eq, map, tryCatch, not, assign } = require("rubico");
-const { first, find, callProp, pluck, isFunction } = require("rubico/x");
+const { Command } = require("commander");
 
-exports.writeToFile =
+const { pipe, tap, get, eq, map, fork, filter, flatMap } = require("rubico");
+const {
+  first,
+  find,
+  callProp,
+  pluck,
+  isFunction,
+  identity,
+} = require("rubico/x");
+
+const writeToFile =
   ({ filename }) =>
   (content) =>
     pipe([
@@ -16,6 +25,104 @@ exports.writeToFile =
         console.log(`written to ${filename}`);
       }),
     ])();
+
+const readModel = (options) =>
+  pipe([
+    tap(() => {
+      console.log(`readModel`, options);
+    }),
+    () => fs.readFile(path.resolve(options.input), "utf-8"),
+    JSON.parse,
+    tap((xxx) => {
+      console.log("");
+    }),
+    get("result.results"),
+    first,
+    tap((xxx) => {
+      console.log("");
+    }),
+    get("results"),
+    tap((xxx) => {
+      console.log("");
+    }),
+  ]);
+
+exports.readModel = readModel;
+
+const readMapping = (options) =>
+  pipe([
+    tap(() => {
+      //console.log("readMapping", options.mapping);
+    }),
+    () => fs.readFile(path.resolve(options.mapping), "utf-8"),
+    JSON.parse,
+  ]);
+
+exports.readMapping = readMapping;
+
+exports.createProgramOptions = ({ version }) => {
+  const program = new Command();
+  program.storeOptionsAsProperties(false);
+  program.allowUnknownOption(); // For testing
+  program.version(version);
+  program.requiredOption("-i, --input <file>", "lives resources");
+  program.option("-o, --outputCode <file>", "iac.js output", "iac.js");
+  program.option("-c, --outputConfig <file>", "config.js output", "config.js");
+  program.option("-m, --mapping <file>", "mapping file", "mapping.json");
+
+  program.parse(process.argv);
+
+  return program.opts();
+};
+
+const writeIac =
+  ({ filename, iacTpl }) =>
+  (resources) =>
+    pipe([
+      () => resources,
+      fork({
+        resourcesVarNames: pluck("resourceVarName"),
+        resourcesCode: pipe([pluck("code"), callProp("join", "\n")]),
+      }),
+      tap((xxx) => {
+        assert(true);
+      }),
+      ({ resourcesVarNames, resourcesCode }) =>
+        iacTpl({ resourcesVarNames, resourcesCode }),
+      writeToFile({ filename }),
+    ])();
+
+const writeConfig =
+  ({ filename, configTpl }) =>
+  (resources) =>
+    pipe([
+      () => resources,
+      pluck("config"),
+      callProp("join", "\n"),
+      tap((xxx) => {
+        assert(true);
+      }),
+      configTpl,
+      writeToFile({ filename }),
+    ])();
+
+exports.generatorMain = ({ name, options, writers, iacTpl, configTpl }) =>
+  pipe([
+    tap((xxx) => {
+      console.log(name);
+    }),
+    fork({ lives: readModel(options), mapping: () => ({}) }),
+    ({ lives, mapping }) =>
+      flatMap((writeResource) => writeResource({ lives, mapping }))(writers),
+    filter(identity),
+    tap((xxx) => {
+      assert(true);
+    }),
+    fork({
+      iac: writeIac({ filename: options.outputCode, iacTpl }),
+      config: writeConfig({ filename: options.outputConfig, configTpl }),
+    }),
+  ])();
 
 exports.ResourceVarName = (name) => camelCase(name);
 
@@ -57,33 +164,3 @@ exports.writeResources =
         ])
       ),
     ])();
-
-exports.readModel = (options) =>
-  pipe([
-    tap(() => {
-      console.log(`readModel`, options);
-    }),
-    () => fs.readFile(path.resolve(options.input), "utf-8"),
-    JSON.parse,
-    tap((xxx) => {
-      console.log("");
-    }),
-    get("result.results"),
-    first,
-    tap((xxx) => {
-      console.log("");
-    }),
-    get("results"),
-    tap((xxx) => {
-      console.log("");
-    }),
-  ]);
-
-exports.readMapping = (options) =>
-  pipe([
-    tap(() => {
-      //console.log("readMapping", options.mapping);
-    }),
-    () => fs.readFile(path.resolve(options.mapping), "utf-8"),
-    JSON.parse,
-  ]);
