c81ccfeba2c3acd6575e830b6b85cfa982d00f36
Frederic Heem <frederic.heem@gmail.com>
2021-08-06 20:18:31 -0300

    eks example aws2gc

---
+++ packages/modules/aws/eks/example/artifacts/config.js
@@ -0,0 +1,465 @@
+module.exports = ({ stage }) => ({
+  projectName: "@grucloud/example-module-aws-eks",
+  iam: {
+    Policy: {
+      amazonEc2ContainerRegistryReadOnly: {
+        name: "AmazonEC2ContainerRegistryReadOnly",
+        properties: {
+          Arn: "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+        },
+      },
+      amazonEksCniPolicy: {
+        name: "AmazonEKS_CNI_Policy",
+        properties: {
+          Arn: "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+        },
+      },
+      amazonEksClusterPolicy: {
+        name: "AmazonEKSClusterPolicy",
+        properties: {
+          Arn: "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+        },
+      },
+      amazonEksvpcResourceController: {
+        name: "AmazonEKSVPCResourceController",
+        properties: {
+          Arn: "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController",
+        },
+      },
+      amazonEksWorkerNodePolicy: {
+        name: "AmazonEKSWorkerNodePolicy",
+        properties: {
+          Arn: "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+        },
+      },
+    },
+    Role: {
+      roleCluster: {
+        name: "role-cluster",
+        properties: {
+          RoleName: "role-cluster",
+          Path: "/",
+          AssumeRolePolicyDocument: {
+            Version: "2012-10-17",
+            Statement: [
+              {
+                Effect: "Allow",
+                Principal: {
+                  Service: "eks.amazonaws.com",
+                },
+                Action: "sts:AssumeRole",
+              },
+            ],
+          },
+        },
+      },
+      roleNodeGroup: {
+        name: "role-node-group",
+        properties: {
+          RoleName: "role-node-group",
+          Path: "/",
+          AssumeRolePolicyDocument: {
+            Version: "2012-10-17",
+            Statement: [
+              {
+                Effect: "Allow",
+                Principal: {
+                  Service: "ec2.amazonaws.com",
+                },
+                Action: "sts:AssumeRole",
+              },
+            ],
+          },
+        },
+      },
+    },
+    InstanceProfile: {
+      eks_54bd8e27_30d1_8860Edb8_968fda4c514e: {
+        name: "eks-54bd8e27-30d1-8860-edb8-968fda4c514e",
+      },
+    },
+  },
+  ec2: {
+    Vpc: {
+      vpc: {
+        name: "vpc",
+        properties: {
+          CidrBlock: "192.168.0.0/16",
+          DnsSupport: true,
+          DnsHostnames: true,
+          Tags: [
+            {
+              Key: "kubernetes.io/cluster/cluster",
+              Value: "shared",
+            },
+          ],
+        },
+      },
+    },
+    Subnet: {
+      subnetPrivateA: {
+        name: "subnet-private-a",
+        properties: {
+          CidrBlock: "192.168.96.0/19",
+          AvailabilityZone: "eu-west-2a",
+          MapPublicIpOnLaunch: false,
+          MapCustomerOwnedIpOnLaunch: false,
+          Tags: [
+            {
+              Key: "kubernetes.io/cluster/cluster",
+              Value: "shared",
+            },
+            {
+              Key: "kubernetes.io/role/internal-elb",
+              Value: "1",
+            },
+          ],
+        },
+      },
+      subnetPrivateB: {
+        name: "subnet-private-b",
+        properties: {
+          CidrBlock: "192.168.128.0/19",
+          AvailabilityZone: "eu-west-2b",
+          MapPublicIpOnLaunch: false,
+          MapCustomerOwnedIpOnLaunch: false,
+          Tags: [
+            {
+              Key: "kubernetes.io/role/internal-elb",
+              Value: "1",
+            },
+            {
+              Key: "kubernetes.io/cluster/cluster",
+              Value: "shared",
+            },
+          ],
+        },
+      },
+      subnetPublicA: {
+        name: "subnet-public-a",
+        properties: {
+          CidrBlock: "192.168.0.0/19",
+          AvailabilityZone: "eu-west-2a",
+          MapPublicIpOnLaunch: false,
+          MapCustomerOwnedIpOnLaunch: false,
+          Tags: [
+            {
+              Key: "kubernetes.io/role/elb",
+              Value: "1",
+            },
+            {
+              Key: "kubernetes.io/cluster/cluster",
+              Value: "shared",
+            },
+          ],
+        },
+      },
+      subnetPublicB: {
+        name: "subnet-public-b",
+        properties: {
+          CidrBlock: "192.168.32.0/19",
+          AvailabilityZone: "eu-west-2b",
+          MapPublicIpOnLaunch: false,
+          MapCustomerOwnedIpOnLaunch: false,
+          Tags: [
+            {
+              Key: "kubernetes.io/role/elb",
+              Value: "1",
+            },
+            {
+              Key: "kubernetes.io/cluster/cluster",
+              Value: "shared",
+            },
+          ],
+        },
+      },
+    },
+    ElasticIpAddress: {
+      iep: {
+        name: "iep",
+      },
+    },
+    InternetGateway: {
+      internetGateway: {
+        name: "internet-gateway",
+      },
+    },
+    NatGateway: {
+      natGateway: {
+        name: "nat-gateway",
+      },
+    },
+    RouteTable: {
+      routeTablePrivateA: {
+        name: "route-table-private-a",
+      },
+      routeTablePrivateB: {
+        name: "route-table-private-b",
+      },
+      routeTablePublic: {
+        name: "route-table-public",
+      },
+    },
+    Route: {
+      routePrivateA: {
+        name: "route-private-a",
+        properties: {
+          DestinationCidrBlock: "0.0.0.0/0",
+        },
+      },
+      routePrivateB: {
+        name: "route-private-b",
+        properties: {
+          DestinationCidrBlock: "0.0.0.0/0",
+        },
+      },
+      routePublic: {
+        name: "route-public",
+        properties: {
+          DestinationCidrBlock: "0.0.0.0/0",
+        },
+      },
+    },
+    SecurityGroup: {
+      eksClusterSgCluster_872092154: {
+        name: "eks-cluster-sg-cluster-872092154",
+        properties: {
+          Description:
+            "EKS created security group applied to ENI that is attached to EKS Control Plane master nodes, as well as any managed workloads.",
+          Tags: [
+            {
+              Key: "aws:eks:cluster-name",
+              Value: "cluster",
+            },
+            {
+              Key: "kubernetes.io/cluster/cluster",
+              Value: "owned",
+            },
+          ],
+        },
+      },
+      securityGroupCluster: {
+        name: "security-group-cluster",
+        properties: {
+          Description: "EKS Cluster Security Group",
+        },
+      },
+      securityGroupNode: {
+        name: "security-group-node",
+        properties: {
+          Description: "SG for the EKS Nodes",
+          Tags: [
+            {
+              Key: "kubernetes.io/cluster/cluster",
+              Value: "owned",
+            },
+          ],
+        },
+      },
+    },
+    SecurityGroupRuleIngress: {
+      eksClusterSgCluster_872092154RuleIngressAll: {
+        name: "eks-cluster-sg-cluster-872092154-rule-ingress-all",
+        properties: {
+          IpPermission: {
+            IpProtocol: "-1",
+            FromPort: -1,
+            ToPort: -1,
+          },
+        },
+      },
+      sgClusterRuleIngressHttps: {
+        name: "sg-cluster-rule-ingress-https",
+        properties: {
+          IpPermission: {
+            IpProtocol: "tcp",
+            FromPort: 443,
+            ToPort: 443,
+            IpRanges: [
+              {
+                CidrIp: "0.0.0.0/0",
+              },
+            ],
+            Ipv6Ranges: [
+              {
+                CidrIpv6: "::/0",
+              },
+            ],
+          },
+        },
+      },
+      sgRuleNodeGroupIngressCluster: {
+        name: "sg-rule-node-group-ingress-cluster",
+        properties: {
+          IpPermission: {
+            IpProtocol: "tcp",
+            FromPort: 1025,
+            ToPort: 65535,
+            IpRanges: [
+              {
+                CidrIp: "0.0.0.0/0",
+              },
+            ],
+            Ipv6Ranges: [
+              {
+                CidrIpv6: "::/0",
+              },
+            ],
+          },
+        },
+      },
+    },
+    SecurityGroupRuleEgress: {
+      sgClusterRuleEgress: {
+        name: "sg-cluster-rule-egress",
+        properties: {
+          IpPermission: {
+            IpProtocol: "tcp",
+            FromPort: 1024,
+            ToPort: 65535,
+            IpRanges: [
+              {
+                CidrIp: "0.0.0.0/0",
+              },
+            ],
+            Ipv6Ranges: [
+              {
+                CidrIpv6: "::/0",
+              },
+            ],
+          },
+        },
+      },
+    },
+    Instance: {
+      nodeGroupPrivateClusterI_00d80c41446af0ecd: {
+        name: "node-group-private-cluster::i-00d80c41446af0ecd",
+        properties: {
+          InstanceType: "t2.medium",
+          ImageId: "ami-0ee353797c2e5f904",
+          Tags: [
+            {
+              Key: "k8s.io/cluster-autoscaler/enabled",
+              Value: "true",
+            },
+            {
+              Key: "eks:nodegroup-name",
+              Value: "node-group-private-cluster",
+            },
+            {
+              Key: "aws:ec2:fleet-id",
+              Value: "fleet-457dea32-c395-d37d-0490-8ba2e164e207",
+            },
+            {
+              Key: "kubernetes.io/cluster/cluster",
+              Value: "owned",
+            },
+            {
+              Key: "aws:autoscaling:groupName",
+              Value: "eks-54bd8e27-30d1-8860-edb8-968fda4c514e",
+            },
+            {
+              Key: "aws:ec2launchtemplate:version",
+              Value: "1",
+            },
+            {
+              Key: "aws:ec2launchtemplate:id",
+              Value: "lt-0e65024cd4128ce96",
+            },
+            {
+              Key: "eks:cluster-name",
+              Value: "cluster",
+            },
+            {
+              Key: "k8s.io/cluster-autoscaler/cluster",
+              Value: "owned",
+            },
+          ],
+        },
+      },
+    },
+  },
+  autoscaling: {
+    AutoScalingGroup: {
+      eks_54bd8e27_30d1_8860Edb8_968fda4c514e: {
+        name: "eks-54bd8e27-30d1-8860-edb8-968fda4c514e",
+        properties: {
+          Tags: [
+            {
+              ResourceId: "eks-54bd8e27-30d1-8860-edb8-968fda4c514e",
+              ResourceType: "auto-scaling-group",
+              Key: "eks:cluster-name",
+              Value: "cluster",
+              PropagateAtLaunch: true,
+            },
+            {
+              ResourceId: "eks-54bd8e27-30d1-8860-edb8-968fda4c514e",
+              ResourceType: "auto-scaling-group",
+              Key: "eks:nodegroup-name",
+              Value: "node-group-private-cluster",
+              PropagateAtLaunch: true,
+            },
+            {
+              ResourceId: "eks-54bd8e27-30d1-8860-edb8-968fda4c514e",
+              ResourceType: "auto-scaling-group",
+              Key: "k8s.io/cluster-autoscaler/cluster",
+              Value: "owned",
+              PropagateAtLaunch: true,
+            },
+            {
+              ResourceId: "eks-54bd8e27-30d1-8860-edb8-968fda4c514e",
+              ResourceType: "auto-scaling-group",
+              Key: "k8s.io/cluster-autoscaler/enabled",
+              Value: "true",
+              PropagateAtLaunch: true,
+            },
+            {
+              ResourceId: "eks-54bd8e27-30d1-8860-edb8-968fda4c514e",
+              ResourceType: "auto-scaling-group",
+              Key: "kubernetes.io/cluster/cluster",
+              Value: "owned",
+              PropagateAtLaunch: true,
+            },
+          ],
+        },
+      },
+    },
+  },
+  kms: {
+    Key: {
+      eksKey: {
+        name: "eks-key",
+      },
+      secretKeyTest: {
+        name: "secret-key-test",
+      },
+    },
+  },
+  eks: {
+    Cluster: {
+      cluster: {
+        name: "cluster",
+        properties: {
+          version: "1.20",
+        },
+      },
+    },
+    NodeGroup: {
+      nodeGroupPrivateCluster: {
+        name: "node-group-private-cluster",
+        properties: {
+          capacityType: "ON_DEMAND",
+          scalingConfig: {
+            minSize: 1,
+            maxSize: 1,
+            desiredSize: 1,
+          },
+          instanceTypes: ["t2.medium"],
+          amiType: "AL2_x86_64",
+          labels: {},
+          diskSize: 20,
+        },
+      },
+    },
+  },
+});
