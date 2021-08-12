a1202965eb1d70b2fd62f9fa4b04517578a3058c
Frederic Heem <frederic.heem@gmail.com>
2021-08-12 19:32:46 -0300

    testEnd2End

--- examples/google/vm-network/test/iac.test.js
+++ examples/google/vm-network/test/iac.test.js
@@ -1,39 +1,14 @@
 const assert = require("assert");
 const path = require("path");
-const { Cli } = require("@grucloud/core/cli/cliCommands");
+const { Cli, testEnd2End } = require("@grucloud/core/cli/cliCommands");
 const { createStack } = require("../iac");
-const config = require("../config");
 
 describe("Gcp VM in network", async function () {
   before(async function () {});
   it("run", async function () {
     const programOptions = { workingDirectory: path.resolve(__dirname, "../") };
-    const cli = await Cli({ programOptions, createStack, config });
+    const cli = await Cli({ programOptions, createStack });
 
-    await cli.graphTree({
-      commandOptions: {},
-    });
-
-    await cli.graphTarget({
-      commandOptions: {},
-    });
-
-    await cli.planDestroy({
-      commandOptions: { force: true },
-    });
-    await cli.planApply({
-      commandOptions: { force: true },
-    });
-    await cli.list({
-      commandOptions: { our: true, graph: true },
-    });
-    await cli.planDestroy({
-      commandOptions: { force: true },
-    });
-    // TODO list should be empty
-    const result = await cli.list({
-      commandOptions: { our: true },
-    });
-    assert(result);
+    await testEnd2End({ cli });
   });
 });
--- examples/k8s/tuto1/test/iac.test.js
+++ examples/k8s/tuto1/test/iac.test.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const path = require("path");
-const { Cli } = require("@grucloud/core/cli/cliCommands");
+const { Cli, testEnd2End } = require("@grucloud/core/cli/cliCommands");
 const { createStack } = require("../iac");
 
 describe("K8s Tuto", async function () {
@@ -9,30 +9,6 @@ describe("K8s Tuto", async function () {
     const programOptions = { workingDirectory: path.resolve(__dirname, "../") };
     const cli = await Cli({ programOptions, createStack });
 
-    await cli.graphTree({
-      commandOptions: {},
-    });
-
-    await cli.graphTarget({
-      commandOptions: {},
-    });
-
-    await cli.planDestroy({
-      commandOptions: { force: true },
-    });
-    await cli.planApply({
-      commandOptions: { force: true },
-    });
-    await cli.list({
-      commandOptions: { our: true, graph: true },
-    });
-    await cli.planDestroy({
-      commandOptions: { force: true },
-    });
-    // TODO list should be empty
-    const result = await cli.list({
-      commandOptions: { our: true },
-    });
-    assert(result);
+    await testEnd2End({ cli });
   });
 });
