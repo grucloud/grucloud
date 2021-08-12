34ac9695be07ae3c319d2898f9c49d4fbe5cd673
Frederic Heem <frederic.heem@gmail.com>
2021-08-12 19:55:07 -0300

    testEnd2End

--- packages/modules/k8s/cert-manager/example/test/iac.test.js
+++ packages/modules/k8s/cert-manager/example/test/iac.test.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 const path = require("path");
 const { ConfigLoader } = require("@grucloud/core/ConfigLoader");
-const { Cli } = require("@grucloud/core/cli/cliCommands");
+const { Cli, testEnd2End } = require("@grucloud/core/cli/cliCommands");
 
 const { createStack } = require("../iac");
 
@@ -18,37 +18,6 @@ describe("K8S Cert Manager Module", async function () {
 
     const cli = await Cli({ programOptions, createStack });
 
-    await cli.graphTree({
-      commandOptions: { full: true },
-    });
-
-    await cli.planDestroy({
-      commandOptions: { force: true },
-    });
-    await cli.planQuery({});
-    await cli.planApply({
-      commandOptions: { force: true },
-    });
-    await cli.planApply({
-      commandOptions: {},
-    });
-    await cli.planQuery({});
-    await cli.planRunScript({
-      commandOptions: { onDeployed: true },
-    });
-    await cli.planDestroy({
-      commandOptions: { force: true },
-    });
-    await cli.planRunScript({
-      commandOptions: { onDestroyed: true },
-    });
-    await cli.planDestroy({
-      commandOptions: {},
-    });
-    // TODO list should be empty
-    const result = await cli.list({
-      commandOptions: { our: true },
-    });
-    assert(result);
+    await testEnd2End({ cli });
   }).timeout(35 * 60e3);
 });
