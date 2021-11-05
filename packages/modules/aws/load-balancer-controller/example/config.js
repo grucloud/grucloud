c3abac6630eed9b6f00a64874b2b3fee2dbf1b9a
Frederic Heem <frederic.heem@gmail.com>
2021-11-05 15:33:38 -0300

    remove load-balancer-controller examples

--- packages/modules/aws/load-balancer-controller/example/config.js
+++
@@ -1,9 +0,0 @@
-const pkg = require("./package.json");
-
-module.exports = ({}) => ({
-  projectName: pkg.name,
-  awsLoadBalancerController: {
-    iamOpenIdConnectProvider: { name: "oidc-eks" },
-    role: { name: "role-load-balancer" },
-  },
-});
