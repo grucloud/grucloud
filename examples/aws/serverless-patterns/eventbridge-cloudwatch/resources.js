a1dc0efa51ab76bad2c570663e2bb2a29a42e678
Frederic Heem <frederic.heem@gmail.com>
2022-04-29 10:06:46 +0200

    lambda function: add message in shouldRetryOnExceptionMessages

--- examples/aws/serverless-patterns/eventbridge-cloudwatch/resources.js
+++ examples/aws/serverless-patterns/eventbridge-cloudwatch/resources.js
@@ -2,18 +2,4 @@
 const {} = require("rubico");
 const {} = require("rubico/x");
 
-exports.createResources = () => [
-  {
-    type: "Document",
-    group: "SSM",
-    properties: ({}) => ({
-      DocumentFormat: "JSON",
-      DocumentType: "Automation",
-      DocumentVersion: "1",
-      Name: "840541460064_serverlessland_automation",
-      Owner: "840541460064",
-      PlatformTypes: ["Windows", "Linux", "MacOS"],
-      SchemaVersion: "0.3",
-    }),
-  },
-];
+exports.createResources = () => [];
