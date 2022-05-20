ccf74b5753bf4547ae339e9cde61a89d12a29720
Frederic Heem <frederic.heem@gmail.com>
2022-05-21 01:14:39 +0200

    gc new: role assignment and register namespace in parallel

--- packages/core/cli/runShellCommands.js
+++ packages/core/cli/runShellCommands.js
@@ -10,72 +10,74 @@ const buildContext = ({ command, index }) => ({
   displayText: () => command,
 });
 
-exports.runShellCommands = ({ text, commands }) =>
-  pipe([
-    tap((params) => {
-      assert(Array.isArray(commands));
-    }),
-    () => ({
-      text,
-      command: ({ onStateChange }) =>
-        pipe([
-          tap(() => {
-            assert(onStateChange);
-          }),
-          () => commands,
-          map.withIndex((command, index) =>
-            pipe([
-              tap(() =>
-                onStateChange({
-                  context: buildContext({ command, index }),
-                  nextState: "WAITING",
-                })
-              ),
-              tap(() =>
-                onStateChange({
-                  context: buildContext({ command, index }),
-                  nextState: "RUNNING",
-                })
-              ),
-              () =>
-                new Promise((resolve, reject) =>
-                  shell.exec(
-                    command,
-                    {
-                      silent: true,
-                      async: true,
-                    },
-                    (code, stdout, stderr) =>
-                      pipe([
-                        switchCase([
-                          () => code === 0,
-                          pipe([
-                            tap(() =>
-                              onStateChange({
-                                context: buildContext({ command, index }),
-                                nextState: "DONE",
-                              })
-                            ),
-                            () => ({ stdout }),
-                            resolve,
-                          ]),
-                          pipe([
-                            tap(() =>
-                              onStateChange({
-                                context: buildContext({ command, index }),
-                                nextState: "ERROR",
-                              })
-                            ),
-                            () => ({ stderr, error: true }),
-                            resolve,
-                          ]),
-                        ]),
-                      ])()
-                  )
+exports.runShellCommands =
+  ({ text }) =>
+  (commands) =>
+    pipe([
+      tap((params) => {
+        assert(Array.isArray(commands));
+      }),
+      () => ({
+        text,
+        command: ({ onStateChange }) =>
+          pipe([
+            tap(() => {
+              assert(onStateChange);
+            }),
+            () => commands,
+            map.withIndex((command, index) =>
+              pipe([
+                tap(() =>
+                  onStateChange({
+                    context: buildContext({ command, index }),
+                    nextState: "WAITING",
+                  })
                 ),
-            ])()
-          ),
-        ])(),
-    }),
-    runAsyncCommand,
-  ])();
+                tap(() =>
+                  onStateChange({
+                    context: buildContext({ command, index }),
+                    nextState: "RUNNING",
+                  })
+                ),
+                () =>
+                  new Promise((resolve, reject) =>
+                    shell.exec(
+                      command,
+                      {
+                        silent: true,
+                        async: true,
+                      },
+                      (code, stdout, stderr) =>
+                        pipe([
+                          switchCase([
+                            () => code === 0,
+                            pipe([
+                              tap(() =>
+                                onStateChange({
+                                  context: buildContext({ command, index }),
+                                  nextState: "DONE",
+                                })
+                              ),
+                              () => ({ stdout }),
+                              resolve,
+                            ]),
+                            pipe([
+                              tap(() =>
+                                onStateChange({
+                                  context: buildContext({ command, index }),
+                                  nextState: "ERROR",
+                                })
+                              ),
+                              () => ({ stderr, error: true }),
+                              resolve,
+                            ]),
+                          ]),
+                        ])()
+                    )
+                  ),
+              ])()
+            ),
+          ])(),
+      }),
+      runAsyncCommand,
+    ])();
--- packages/core/cli/test/runShellCommands.test.js
+++ packages/core/cli/test/runShellCommands.test.js
@@ -7,18 +7,17 @@ describe("runShellCommands", function () {
     try {
       await runShellCommands({
         text: "register",
-        commands: ["sleep 2", "sleep 1"],
-      });
+      })(["sleep 2", "sleep 1"]);
     } catch (error) {
       assert(false);
     }
   });
-  it.only("throw", async function () {
+  it("throw", async function () {
     try {
       await runShellCommands({
         text: "register",
         commands: ["exit -1", "sleep 1"],
-      });
+      })(["exit -1", "sleep 1"]);
     } catch (error) {
       assert(false);
     }
