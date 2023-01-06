67c7ff077d60197e35d86481ccee84167ce66cf2
Frederic Heem <frederic.heem@gmail.com>
2023-01-06 09:38:53 -0500

    fix FirewallLoggingConfiguration

--- examples/aws/WAFv2/wafv2-apigateway-rest/resources.js
+++ examples/aws/WAFv2/wafv2-apigateway-rest/resources.js
@@ -62,8 +62,36 @@ exports.createResources = () => [
                       "method.response.header.Content-Type": "'text/html'",
                     },
                     responseTemplates: {
-                      "text/html":
-                        '<html>\n    <head>\n        <style>\n        body {\n            color: #333;\n            font-family: Sans-serif;\n            max-width: 800px;\n            margin: auto;\n        }\n        </style>\n    </head>\n    <body>\n        <h1>Welcome to your Pet Store API</h1>\n        <p>\n            You have successfully deployed your first API. You are seeing this HTML page because the <code>GET</code> method to the root resource of your API returns this content as a Mock integration.\n        </p>\n        <p>\n            The Pet Store API contains the <code>/pets</code> and <code>/pets/{petId}</code> resources. By making a <a href="/$context.stage/pets/" target="_blank"><code>GET</code> request</a> to <code>/pets</code> you can retrieve a list of Pets in your API. If you are looking for a specific pet, for example the pet with ID 1, you can make a <a href="/$context.stage/pets/1" target="_blank"><code>GET</code> request</a> to <code>/pets/1</code>.\n        </p>\n        <p>\n            You can use a REST client such as <a href="https://www.getpostman.com/" target="_blank">Postman</a> to test the <code>POST</code> methods in your API to create a new pet. Use the sample body below to send the <code>POST</code> request:\n        </p>\n        <pre>\n{\n    "type" : "cat",\n    "price" : 123.11\n}\n        </pre>\n    </body>\n</html>',
+                      "text/html": `<html>
+    <head>
+        <style>
+        body {
+            color: #333;
+            font-family: Sans-serif;
+            max-width: 800px;
+            margin: auto;
+        }
+        </style>
+    </head>
+    <body>
+        <h1>Welcome to your Pet Store API</h1>
+        <p>
+            You have successfully deployed your first API. You are seeing this HTML page because the <code>GET</code> method to the root resource of your API returns this content as a Mock integration.
+        </p>
+        <p>
+            The Pet Store API contains the <code>/pets</code> and <code>/pets/{petId}</code> resources. By making a <a href="/$context.stage/pets/" target="_blank"><code>GET</code> request</a> to <code>/pets</code> you can retrieve a list of Pets in your API. If you are looking for a specific pet, for example the pet with ID 1, you can make a <a href="/$context.stage/pets/1" target="_blank"><code>GET</code> request</a> to <code>/pets/1</code>.
+        </p>
+        <p>
+            You can use a REST client such as <a href="https://www.getpostman.com/" target="_blank">Postman</a> to test the <code>POST</code> methods in your API to create a new pet. Use the sample body below to send the <code>POST</code> request:
+        </p>
+        <pre>
+{
+    "type" : "cat",
+    "price" : 123.11
+}
+        </pre>
+    </body>
+</html>`,
                     },
                     statusCode: "200",
                   },
