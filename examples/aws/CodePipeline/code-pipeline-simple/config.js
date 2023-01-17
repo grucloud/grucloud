6f729752182454b500062d1691bd7681bb678075
Frederic Heem <frederic.heem@gmail.com>
2023-01-17 10:43:13 -0500

    more includeGroups

--- examples/aws/CodePipeline/code-pipeline-simple/config.js
+++ examples/aws/CodePipeline/code-pipeline-simple/config.js
@@ -1,4 +1,11 @@
 const pkg = require("./package.json");
 module.exports = () => ({
   projectName: pkg.name,
+  includeGroups: [
+    "CloudWatchLogs",
+    "CodeBuild",
+    "CodePipeline",
+    "CodeStarConnections",
+    "S3",
+  ],
 });
