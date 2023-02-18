2cbeacf2d0a539def1644a34d8acee15548834d7
Frederic Heem <frederic.heem@gmail.com>
2023-02-17 22:40:33 -0500

    s3-lambda s3-lambda-dynamodb-cdk s3-lambda-mediaconvert

--- examples/aws/CloudWatchEvent/s3-lambda/config.js
+++ examples/aws/CloudWatchEvent/s3-lambda/config.js
@@ -1,5 +1,5 @@
 const pkg = require("./package.json");
 module.exports = () => ({
   projectName: pkg.name,
-  includeGroups: ["CloudWatchEvents", "Lambda", "S3"],
+  includeGroups: ["CloudWatchEvents", "IAM", "Lambda", "S3"],
 });
---
+++ examples/aws/S3/s3-lambda-dynamodb-cdk/README.md
@@ -0,0 +1,3 @@
+# Aws S3 Lambda DynamoDB
+
+See https://github.com/aws-samples/serverless-patterns/tree/main/s3-lambda-dynamodb-cdk
