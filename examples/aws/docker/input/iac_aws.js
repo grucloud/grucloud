85c5b508c1bd4b26061893fa6524982dcace9a4a
Frederic Heem <frederic.heem@gmail.com>
2023-09-07 12:45:57 -0300

    azure list and info

--- examples/aws/docker/input/iac_aws.js
+++ examples/aws/docker/input/iac_aws.js
@@ -1,5 +1,9 @@
 exports.createStack = () => ({
   providerFactory: require("@grucloud/provider-aws").AwsProvider,
   createResources: () => [],
-  config: () => ({ region: process.env.AWS_REGION, includeAllResources: true }),
+  config: () => ({
+    region: process.env.AWS_REGION,
+    //
+    //includeAllResources: true,
+  }),
 });
