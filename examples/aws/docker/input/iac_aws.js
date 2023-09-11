4048cd34662d09232e258374825674b9d8b8ef47
Frederic Heem <frederic.heem@gmail.com>
2023-09-11 00:15:24 -0300

    dockerize ok

--- examples/aws/docker/input/iac_aws.js
+++
@@ -1,9 +0,0 @@
-exports.createStack = () => ({
-  providerFactory: require("@grucloud/provider-aws").AwsProvider,
-  createResources: () => [],
-  config: () => ({
-    region: process.env.AWS_REGION,
-    //
-    //includeAllResources: true,
-  }),
-});
