6c7063364bb6ad3f6c375980f13a34ae68499693
Frederic Heem <frederic.heem@gmail.com>
2023-10-06 18:30:00 -0300

    S3_AWSAccessKeyId, S3_AWSSecretKey and S3_AWS_REGION

--- packages/core/cli/test/uploadDirToS3.test.js
+++ packages/core/cli/test/uploadDirToS3.test.js
@@ -1,8 +1,13 @@
 const assert = require("assert");
-const { uploadDirToS3 } = require("..//uploadDirToS3");
+const { uploadDirToS3 } = require("../uploadDirToS3");
 const Path = require("node:path");
+
 describe("UploadDirToS3", function () {
-  before(async function () {});
+  before(async function () {
+    if (!process.env.S3_AWS_REGION) {
+      this.skip();
+    }
+  });
 
   it("upload", async () => {
     try {
